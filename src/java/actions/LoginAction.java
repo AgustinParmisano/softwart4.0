3dd17115269e6a9e4d85f0e3b8781ee69e1954a2
Agustin Parmisano <agustinparmisano@gmail.com>
2014-04-26 19:37:21 -0300

    Agregadas las entidades con sus respectivas anotaciones

--- src/java/actions/LoginAction.java
+++ src/java/actions/LoginAction.java
@@ -10,13 +10,51 @@ package actions;
  *
  * @author agustin
  */
-public class LoginAction {
+public class LoginAction{
+    String identificacion;
+    String clave;
+    String usuario;
+    String perfil;
     
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+ 
     public LoginAction() {
     }
     
     public String execute() throws Exception {
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("Entro a LoginAction execute");
+        
         return "success";
     }
     
